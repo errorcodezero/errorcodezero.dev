120687f5a84f22f519c1014d845b7baf338a01b7
ErrorCode0 <me@errorcodezero.dev>
2024-02-03 09:50:58 -0800

    Initial Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import catppuccin from "@catppuccin/tailwindcss"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+        catppuccin({
+            prefix: "ctp",
+            defaultFlavour: "mocha"
+        })
+    ],
+}
