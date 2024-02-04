a876210cf3281f89a9438b84c56d8d0a6f9e5417
ErrorCode0 <me@errorcodezero.dev>
2024-02-03 21:36:36 -0800

    Adding robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.errorcodezero.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()],
   markdown: {
     shikiConfig: {
       theme: "catppuccin-mocha"
