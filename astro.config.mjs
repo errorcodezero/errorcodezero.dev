0cc7710b6bd1cd598453c8e6c19118ccae7eefb0
ErrorCode0 <me@errorcodezero.dev>
2024-04-16 15:35:37 -0700

    Adding wip footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import robotsTxt from "astro-robots-txt";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.errorcodezero.dev',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), icon()],
   markdown: {
     shikiConfig: {
-      theme: "catppuccin-mocha",
+      theme: "catppuccin-mocha"
     }
   }
 });
