ff2f5574338bca7931f35c11263d6cb525e69ee3
ErrorCode0 <me@errorcodezero.dev>
2024-04-19 10:07:23 -0700

    Revert "Adding vite-pwa(should work?)"
    This reverts commit d5c489e2b65482e4d1d161d99ef19f72ac6afc8f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-import icon from "astro-icon";
 
-import vitePwa from "@vite-pwa/astro";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.errorcodezero.dev',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), icon(), vitePwa({
-    mode: "production",
-    registerType: "autoUpdate",
-    includeAssets: "logo.png",
-    workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-    },
-    manifest: {
-        name: "ErrorCode0",
-        theme_color: "#89b4fa"
-    }
-  })],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), icon()],
   markdown: {
     shikiConfig: {
       theme: "catppuccin-mocha"
