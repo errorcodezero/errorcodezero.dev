d5c489e2b65482e4d1d161d99ef19f72ac6afc8f
ErrorCode0 <me@errorcodezero.dev>
2024-04-19 09:18:55 -0700

    Adding vite-pwa(should work?)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,25 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-
 import icon from "astro-icon";
+
+import vitePwa from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.errorcodezero.dev',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), icon()],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), icon(), vitePwa({
+    mode: "production",
+    registerType: "autoUpdate",
+    includeAssets: "logo.png",
+    workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    manifest: {
+        name: "ErrorCode0",
+        theme_color: "#89b4fa"
+    }
+  })],
   markdown: {
     shikiConfig: {
       theme: "catppuccin-mocha"
