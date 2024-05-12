1b19ba07da270b2f9d4faefe61c99ec4728f36cb
ErrorCode0 <me@errorcodezero.dev>
2024-05-12 09:46:27 -0700

    Adding cloudflare analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-
 import icon from "astro-icon";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.errorcodezero.dev',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), icon()],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), icon(), partytown()],
   markdown: {
     shikiConfig: {
       theme: "catppuccin-mocha"
