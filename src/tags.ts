dc62b9b37bbc5fce432dfa7511dd7cf4a360edd1
ErrorCode0 <me@errorcodezero.dev>
2024-05-15 14:49:13 -0700

    Making so blog post tags lead to tags page

--- src/tags.ts
+++ src/tags.ts
@@ -1,14 +1,14 @@
 const tags = [
-  ["linux", "text-ctp-yellow"],
-  ["typescript", "text-ctp-blue"],
-  ["hackathon", "text-ctp-green"],
-  ["neovim", "text-ctp-green"],
-  ["IT", "text-ctp-mauve"],
-  ["laptop", "text-ctp-blue"],
-  ["astro", "text-ctp-red"],
-  ["test", "text-ctp-orange"],
-  ["tech-stack", "text-ctp-blue"],
-  ["gamedev", "text-ctp-sky"]
+  ["linux", "!text-ctp-yellow"],
+  ["typescript", "!text-ctp-blue"],
+  ["hackathon", "!text-ctp-green"],
+  ["neovim", "!text-ctp-green"],
+  ["IT", "!text-ctp-mauve"],
+  ["laptop", "!text-ctp-blue"],
+  ["astro", "!text-ctp-red"],
+  ["test", "!text-ctp-orange"],
+  ["tech-stack", "!text-ctp-blue"],
+  ["gamedev", "!text-ctp-sky"]
 ];
 
 export default tags;
