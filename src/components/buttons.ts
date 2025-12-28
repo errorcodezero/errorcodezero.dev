62466e5b5fda530851993c12d509dc619a306026
ErrorCode0 <me@errorcodezero.dev>
2025-12-28 13:29:07 +0530

    added vic's 88x31 to my site

---
+++ src/components/buttons.ts
@@ -0,0 +1,17 @@
+export const buttons = [{
+	src: "/button.png",
+	link: "/",
+	alt: "ErrorCode0's Button",
+}, {
+	src: "https://bomberfish.ca/button.gif",
+	link: "https://bomberfish.ca/",
+	alt: "BomberFish's Button",
+}, {
+	src: "https://foxmoss.com/foxmossbutton.png",
+	link: "https://foxmoss.com",
+	alt: "Foxmoss's Button",
+}, {
+	src: "https://v1c.rocks/88x31.png",
+	link: "https://v1c.rocks",
+	alt: "Vic's Button",
+}];
