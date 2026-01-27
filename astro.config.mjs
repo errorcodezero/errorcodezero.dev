12c4de1c2db535c4675c2e3da51c368c68e36ba5
ErrorCode0 <me@errorcodezero.dev>
2026-01-27 02:33:18 -0800

    cutting tailwind loose finally!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import icon from "astro-icon";
 
@@ -13,7 +12,6 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap(),
-    tailwind(),
     robotsTxt(),
     icon(),
     partytown(),
