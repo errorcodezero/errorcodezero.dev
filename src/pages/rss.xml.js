44affc14aa224007b5fee1a840cd983192b92f05
ErrorCode0 <me@errorcodezero.dev>
2026-01-31 16:49:26 -0800

    fixed rss feed oops

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,6 +4,11 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "~/consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].data.draft) {
+      posts.splice(i, 1);
+    }
+  }
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
