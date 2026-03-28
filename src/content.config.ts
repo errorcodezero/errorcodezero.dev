99b1bb05ba72d154126a98ba05803f94b883ffca
ErrorCode0 <me@errorcodezero.dev>
2026-03-27 22:18:54 -0700

    adding cloudflare analytics

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,18 +3,17 @@ import { z } from "astro/zod";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  // loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./blog" }),
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  schema: () =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      // Transform string to Date object
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      categories: z.array(z.string()),
-      draft: z.boolean().optional(),
-    }),
+	// loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./blog" }),
+	loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+	schema: () =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			draft: z.boolean().optional(),
+		}),
 });
 
 export const collections = { blog };
---
+++ src/content/blog/taking-back-my-life.md
@@ -0,0 +1,14 @@
+---
+title: "Taking back my life"
+description: "Escape the clutches that your devices have on you."
+pubDate: "March 22 2026"
+updatedDate: "March 22 2026"
+categories: ["storytelling", "selfhelp"]
+draft: true
+---
+
+![a diagram of tanks with pipes coming from a main tank labeled "attention". the "social media" and "video games" tanks are kept significantly more filled than the "reading", "friends", "family", and "work" tanks down below.](../../assets/attention-monopoly.png)
+
+> If every minute you spent scrolling was instead spent by you reading or learning, how much smarter do you think you would be?
+
+I thought of that question and it rocked me to my core for a bit. I definitely had a scrolling problem.
