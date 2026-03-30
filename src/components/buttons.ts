1f2306e53a57e847ed95fe3c3cf346308098996f
ErrorCode0 <me@errorcodezero.dev>
2026-03-29 17:45:23 -0700

    replacing my button with a gif version(cooler)

--- src/components/buttons.ts
+++ src/components/buttons.ts
@@ -1,37 +1,37 @@
 export const buttons = [
-  {
-    src: "/button.png",
-    link: "/",
-    alt: "ErrorCode0's Button",
-  },
-  {
-    src: "https://bomberfish.ca/button.gif",
-    link: "https://bomberfish.ca/",
-    alt: "BomberFish's Button",
-  },
-  {
-    src: "https://foxmoss.com/foxmossbutton.png",
-    link: "https://foxmoss.com",
-    alt: "Foxmoss's Button",
-  },
-  {
-    src: "https://v1c.rocks/88x31.png",
-    link: "https://v1c.rocks",
-    alt: "Vic's Button",
-  },
-  {
-    src: "https://github.com/aelithron/novatea/blob/main/public/88x31.gif?raw=true",
-    link: "https://novatea.dev",
-    alt: "Nova's Button",
-  },
-  {
-    src: "https://www.dwait.dev/button.jpg",
-    link: "https://www.dwait.dev",
-    alt: "Dwait's Button",
-  },
-  {
-    src: "https://www.addy10s.xyz/addy88x31.gif",
-    link: "https://www.addy10s.xyz/",
-    alt: "Addy's Button",
-  },
+	{
+		src: "/button.gif",
+		link: "/",
+		alt: "ErrorCode0's Button",
+	},
+	{
+		src: "https://bomberfish.ca/button.gif",
+		link: "https://bomberfish.ca/",
+		alt: "BomberFish's Button",
+	},
+	{
+		src: "https://foxmoss.com/foxmossbutton.png",
+		link: "https://foxmoss.com",
+		alt: "Foxmoss's Button",
+	},
+	{
+		src: "https://v1c.rocks/88x31.png",
+		link: "https://v1c.rocks",
+		alt: "Vic's Button",
+	},
+	{
+		src: "https://github.com/aelithron/novatea/blob/main/public/88x31.gif?raw=true",
+		link: "https://novatea.dev",
+		alt: "Nova's Button",
+	},
+	{
+		src: "https://www.dwait.dev/button.jpg",
+		link: "https://www.dwait.dev",
+		alt: "Dwait's Button",
+	},
+	{
+		src: "https://www.addy10s.xyz/addy88x31.gif",
+		link: "https://www.addy10s.xyz/",
+		alt: "Addy's Button",
+	},
 ];
